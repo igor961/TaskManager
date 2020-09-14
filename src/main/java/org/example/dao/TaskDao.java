6e16efb93f5cd8a7a790f604f14384959c395937
Igor <igor.shesterikov2017@gmail.com>
2020-09-14 14:13:58 +0300

    fix update query for task (add priority field)

--- src/main/java/org/example/dao/TaskDao.java
+++ src/main/java/org/example/dao/TaskDao.java
@@ -67,7 +67,7 @@ public class TaskDao implements BasicDao<TaskDto> {
 
     @Override
     public void update(TaskDto task) {
-        jdbcTemplate.update("UPDATE tasks SET name = ?, status = ? WHERE id = ?;", task.name, task.status, task.id);
+        jdbcTemplate.update("UPDATE tasks SET name = ?, status = ?, priority = ? WHERE id = ?;", task.name, task.status, task.priority, task.id);
     }
 
     @Override
